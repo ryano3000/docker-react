acf08fb50c6ac82cf691846f81720ed8277ce124
Ryan <ryan@captain.ai>
2018-12-26 21:07:06 -0500

    frontend

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-it('renders without crashing', () => {
+it('renders witho5ut crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
+
